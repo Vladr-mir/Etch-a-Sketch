9da67465868e078dd416dec742a789074b83c7f5
Vladimir <duenasvladimir@gmail.com>
2022-07-15 00:16:52 -0600

    Added UI functionality

--- main.js
+++ main.js
@@ -6,21 +6,59 @@
 let color = document.querySelector('#colorpicker').value;
 let colorMode = 'c';
 let canvasSize = 64;
+
 const sliderLabel = document.querySelector('#range-value');
 const slider = document.querySelector('#canvas-size');
+const colorModeBtn = document.querySelectorAll('.color-mode');
+const resetBtn = document.querySelector('#reset');
+const changeSize = document.querySelector('#reset-size');
+
+addPixels(canvasSize);
+pickColor();
+
 slider.addEventListener('input', () => {
   sliderLabel.textContent = slider.value;
+  canvasSize = parseInt(slider.value);
+});
+
+colorModeBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    removePressedClass();
+    btn.classList.add('pressed');
+    colorMode = btn.dataset.mode;
+  });
+});
+
+resetBtn.addEventListener('click', () => {
+  resetCanvas();
+});
+
+changeSize.addEventListener('click', () => {
+  resetCanvas(canvasSize);
 })
-addPixels(canvasSize);
-preventDrag();
-pickColor();
-paintOnClick();
+
+function removePressedClass() {
+  colorModeBtn.forEach(btn => btn.classList.remove('pressed'));
+}
 
 function paintOnClick() {
   const pixels = document.querySelectorAll('.pixel');
   pixels.forEach(pixel => {
     pixel.addEventListener('mouseover', (e) => {
-      if (e.buttons > 0) pixel.style.backgroundColor = color; 
+      switch (colorMode) {
+        case 'c':
+          if (e.buttons > 0) pixel.style.backgroundColor = color; 
+          break;
+        case 'i':
+          if (e.buttons > 0) pixel.style.backgroundColor = 'black';
+          break;
+        case 'e':
+          if (e.buttons > 0) pixel.style.backgroundColor = '';
+          break;
+        case 'r':
+          if (e.buttons > 0) pixel.style.backgroundColor = getRandomColor();
+          break;
+      }
     });
   });
 }
@@ -47,6 +85,12 @@ function addPixels(dimensions) {
     }
     sketchArea.appendChild(column);
   }
+  preventDrag();
+  paintOnClick();
+}
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
 function preventDrag() {
@@ -55,4 +99,12 @@ function preventDrag() {
     div.addEventListener('dragstart', (e) => e.preventDefault());
     div.addEventListener('drop', (e) => e.preventDefault());
   });
+}
+
+function resetCanvas(dimensions=64) {
+  const sketchArea = document.querySelector('div.sketch-area');
+  while (sketchArea.firstChild) {
+    sketchArea.removeChild(sketchArea.firstChild);
+  }
+  addPixels(dimensions);
 }
